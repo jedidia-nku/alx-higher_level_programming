2cb3934fd024ea4efbe561456ec3d20a30911d4e
jedidia-nku <jedinkuze123@gmail.com>
2024-01-08 21:03:26 +0000

    All tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines available attributes/methods of an object."""
 
-
 def lookup(obj):
-        """Return a list of available attributes."""
-            return (dir(obj))
+"""Return a list of available attributes."""
+return (dir(obj))
