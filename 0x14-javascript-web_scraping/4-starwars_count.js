8312fb4bbca2b628cf3ed42a717c37b09b8be68a
jedidia-nku <jedinkuze123@gmail.com>
2024-04-30 15:22:48 +0000

    Completed Tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,22 @@
-<<<<<<< HEAD
 t request = require('request');
-=======
-#!/usr/bin/node
-
-const request = require('request');
->>>>>>> 5ea10fcd83d3e5350e69fe8353e614a0cdb15142
+const process = require('process');
 
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-  } else {
-    const films = JSON.parse(body).results;
-    const characterId = '18';
-    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+request(url, function (err, response, body) {
+  if (err) return;
+  if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    let count = 0;
+    for (const item of data.results) {
+      for (const actor of item.characters) {
+        const actorSplit = actor.split('/');
+        const actorId = actorSplit[actorSplit.length - 2];
+        if (actorId === '18') {
+          count++;
+        }
+      }
+    }
     console.log(count);
   }
 });
