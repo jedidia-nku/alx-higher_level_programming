d7c01bef07b53d8f1ba4e978555834adb363def4
jedidia-nku <jedinkuze123@gmail.com>
2024-05-04 22:56:25 +0000

    Tasks completed

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,44 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const movieId = process.argv[2];
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-//  the 'request' module to perform an HTTP GET request to the Star Wars API URL.
-request(apiUrl, function (error, response, body) {
-  // Check if there was no error during the HTTP request
-  if (!error && response.statusCode === 200) {
-    // Parse the JSON response bod
-    const movieData = JSON.parse(body);
-    // create an array of promises that fetch the data for each individual character.
-    const characterPromises = movieData.characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        // Use another 'request' to fetch the data for the individual character.
-        request(characterUrl, function (charError, charResponse, charBody) {
-          // Check if there was no error during the HTTP request
-          if (!charError && charResponse.statusCode === 200) {
-            // Parse the JSON response body
-            const characterData = JSON.parse(charBody);
-            // Resolve the promise with the name of the character.
-            resolve(characterData.name);
-          } else {
-            // reject the promise with the error message if  there was an error during the HTTP request
-            reject(new Error(`Error fetching character data: ${charError}`));
-          }
-        });
-      });
-    });
-
-    Promise.all(characterPromises)
-      .then((characterNames) => {
-        console.log(characterNames.join('\n'));
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-  } else {
-    console.error('Error fetching movie data:', error);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,12 @@
-t request = require('request');
-const process = require('process');
-
-const url = process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err) return;
-  if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    let count = 0;
-    for (const item of data.results) {
-      for (const actor of item.characters) {
-        const actorSplit = actor.split('/');
-        const actorId = actorSplit[actorSplit.length - 2];
-        if (actorId === '18') {
-          count++;
-        }
-      }
-    }
-    console.log(count);
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
